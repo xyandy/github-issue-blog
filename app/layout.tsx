8f659d8094d4b32ea15b3679a47f6e4178dd3911
xuyang1996 <xy913741894@qq.com>
2024-10-07 19:18:27 +0800

    add labels

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
-import { Menu } from 'lucide-react';
+import { getAllLabels } from '@/lib/github';
+import GithubLabel from '@/components/GithubLabel';
 import Link from 'next/link';
 
 import '@/app/globals.css';
@@ -14,9 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <body className='flex flex-col min-h-screen bg-gray-100'>
         <Header />
-
-        <main className='flex-grow py-8'>{children}</main>
-
+        <Main>{children}</Main>
         <Footer />
       </body>
     </html>
@@ -26,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 function Header() {
   return (
     <header className='bg-white shadow-md'>
-      <div className='flex justify-between items-center px-8 py-4'>
+      <div className='flex justify-between items-center px-8 py-3'>
         <h1 className='text-2xl font-bold'>Dogxy Blog</h1>
 
         <nav className='hidden sm:flex items-center space-x-6'>
@@ -50,21 +49,79 @@ function Header() {
           </Link>
         </nav>
 
-        <div className='sm:hidden'>
+        {/* <div className='sm:hidden'>
           <button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
             <span className='sr-only'>打开主菜单</span>
             <Menu className='block h-6 w-6' aria-hidden='true' />
           </button>
-        </div>
+        </div> */}
+
+        <button className='md:hidden'>
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
+          </svg>
+        </button>
       </div>
     </header>
+  );
+}
+
+async function Main({ children }: { children: React.ReactNode }) {
+  // return (
+  //   <main className='flex-grow container mx-auto px-4 py-8'>
+  //     <div className='flex flex-col md:flex-row'>
+  //       {children}
+  //       <aside className='w-full md:w-1/3 space-y-8 md:space-y-4 hidden md:block'>
+  //         <div className='bg-white p-6 rounded-lg shadow'></div>
+
+  //         <div className='bg-white p-6 rounded-lg shadow'>
+  //           <h3 className='text-lg font-semibold mb-2 flex items-center'>
+  //             <span className='bg-red-500 text-white p-1 rounded mr-2'>公告</span>
+  //             公告
+  //           </h3>
+  //           <p className='text-gray-600'>人生就像品茗，慢慢品尝，才能回甘。</p>
+  //         </div>
+
+  //         <div className='bg-white p-6 rounded-lg shadow'>
+  //           <h3 className='text-lg font-semibold mb-4'>最新文章</h3>
+  //         </div>
+  //       </aside>
+  //     </div>
+  //   </main>
+  // );
+  const labels = await getAllLabels();
+  return (
+    <main className='container mx-auto py-8 flex-1 flex'>
+      <div className='md:w-3/4 w-full'>{children}</div>
+
+      <div className='md:w-1/4 hidden md:block space-y-2'>
+        <div className='bg-white p-6 rounded-lg shadow'>
+          <h1 className='text-xl font-semibold mb-4'>标签</h1>
+
+          <div className='flex flex-wrap gap-2'>
+            {labels.map((label: string) => (
+              <Link key={label} href={`/?label=${encodeURIComponent(label)}`}>
+                <GithubLabel key={label} label={label} />
+              </Link>
+            ))}
+          </div>
+        </div>
+        {/* <div className='bg-white p-6 rounded-lg shadow'></div> */}
+      </div>
+    </main>
   );
 }
 
 function Footer() {
   return (
     <footer className='bg-blue-500 shadow'>
-      <div className='container mx-auto text-center p-4 text-white text-lg'>
+      <div className='container mx-auto text-center p-3 text-white text-lg'>
         © {new Date().getFullYear()} GitHub Issue Blog
       </div>
     </footer>
